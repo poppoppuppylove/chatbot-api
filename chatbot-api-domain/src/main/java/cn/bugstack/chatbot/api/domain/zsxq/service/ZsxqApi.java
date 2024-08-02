a25e938cd79428bbf06e7bdf2d62949623c22634
poppoppuppylove <431792974@qq.com>
2024-08-03 04:00:36 +0800

    feat:zsxq接口领域服务开发

---
+++ chatbot-api-domain/src/main/java/cn/bugstack/chatbot/api/domain/zsxq/service/ZsxqApi.java
@@ -0,0 +1,94 @@
+package cn.bugstack.chatbot.api.domain.zsxq.service;
+
+import cn.bugstack.chatbot.api.domain.zsxq.IZsxqApi;
+import cn.bugstack.chatbot.api.domain.zsxq.model.aggregates.UnAnsweredQuestionsAggregates;
+import cn.bugstack.chatbot.api.domain.zsxq.model.req.AnswerReq;
+import cn.bugstack.chatbot.api.domain.zsxq.model.req.ReqData;
+import cn.bugstack.chatbot.api.domain.zsxq.model.res.AnswerRes;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ContentType;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.util.EntityUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+/**
+ * @author 小傅哥，微信：fustack
+ * @description
+ * @github https://github.com/fuzhengwei
+ * @Copyright 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ */
+@Service
+public class ZsxqApi implements IZsxqApi {
+
+    private Logger logger = LoggerFactory.getLogger(ZsxqApi.class);
+
+    @Override
+    public UnAnsweredQuestionsAggregates queryUnAnsweredQuestionsTopicId(String groupId, String cookie) throws IOException {
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpGet get = new HttpGet("https://api.zsxq.com/v2/groups/" + groupId + "/topics?scope=unanswered_questions&count=20");
+
+        get.addHeader("cookie", cookie);
+        get.addHeader("Content-Type", "application/json;charset=utf8");
+
+        CloseableHttpResponse response = httpClient.execute(get);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String jsonStr = EntityUtils.toString(response.getEntity());
+            logger.info("拉取提问数据。groupId：{} jsonStr：{}", groupId, jsonStr);
+            return JSON.parseObject(jsonStr, UnAnsweredQuestionsAggregates.class);
+        } else {
+            throw new RuntimeException("queryUnAnsweredQuestionsTopicId Err Code is " + response.getStatusLine().getStatusCode());
+        }
+    }
+
+    @Override
+    public boolean answer(String groupId, String cookie, String topicId, String text, boolean silenced) throws IOException {
+
+        CloseableHttpClient httpClient = HttpClientBuilder.create().build();
+
+        HttpPost post = new HttpPost("https://api.zsxq.com/v2/topics/" + topicId + "/answer");
+        post.addHeader("cookie", cookie);
+        post.addHeader("Content-Type", "application/json;charset=utf8");
+        post.addHeader("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36");
+//这个HTTP头用于告诉服务器客户端的类型和版本信息（"user-agent"头信息通常用于描述客户端（浏览器）的类型、操作系统、浏览器版本等）
+//在这个例子中，这个HTTP头表示一个运行在Macintosh操作系统上的Chrome浏览器，其版本为109.0.0.0。
+        /* 测试数据
+          String paramJson = "{\n" +
+                "  \"req_data\": {\n" +
+                "    \"text\": \"自己去百度！\\n\",\n" +
+                "    \"image_ids\": [],\n" +
+                "    \"silenced\": false\n" +
+                "  }\n" +
+                "}";
+         */
+
+        AnswerReq answerReq = new AnswerReq(new ReqData(text, silenced));
+        String paramJson = JSONObject.toJSONString(answerReq);//在HTTP请求中，数据通常以JSON格式发送。需要将Java对象转换为JSON字符串，以便在请求体中传递
+
+        StringEntity stringEntity = new StringEntity(paramJson, ContentType.create("text/json", "UTF-8"));//将JSON字符串包装成StringEntity，并设置为HTTP请求的实体
+        post.setEntity(stringEntity);//将JSON字符串直接作为请求体发送可能会导致一些问题，StringEntity对象可以自动处理这些问题
+
+        CloseableHttpResponse response = httpClient.execute(post);
+        if (response.getStatusLine().getStatusCode() == HttpStatus.SC_OK) {
+            String jsonStr = EntityUtils.toString(response.getEntity());//获取HTTP响应的内容，以字符串形式处理，便于后续解析
+            logger.info("回答问题结果。groupId：{} topicId：{} jsonStr：{}", groupId, topicId, jsonStr);
+            AnswerRes answerRes = JSON.parseObject(jsonStr, AnswerRes.class);//将响应字符串解析为Java对象，方便代码中的逻辑处理和数据访问
+            //直接从字符串变成AnswerRes对象是不可行的，因为字符串只是纯文本数据，无法直接映射为Java对象。必须先将字符串解析为JSON对象，然后才能转换为对应的Java对象
+            return answerRes.isSucceeded();
+        } else {
+            throw new RuntimeException("answer Err Code is " + response.getStatusLine().getStatusCode());
+        }
+    }
+
+}
